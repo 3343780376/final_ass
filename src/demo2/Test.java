2adffd55581a774394db1e0f95e96d18a242e298
3343780376 <3343780376@qq.com>
2020-12-27 17:07:58 +0800

    Update Test.java

--- src/demo2/Test.java
+++ src/demo2/Test.java
@@ -5,7 +5,7 @@ public class Test {
         int x=0,y=0,z=0;  //公鸡x只，母鸡y只，小鸡3z只
         for (x = 0; x <30 ; x++) {
             for (y = 0; y < 20; y++) {
-                for (z = 0; z < 100; z++) {
+                for (z = 0; z < 34; z++) {
                     if((3*x+5*y+z==100)&&(x+y+3*z==100)){
                         System.out.println("此时公鸡"+x+"只，母鸡"+y+"只，小鸡"+3*z+"只。");
                     }
